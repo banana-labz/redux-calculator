6b55b0f0d5bc41c9d481b322961fb78fa6f2929d
thxrion <strike201420141@gmail.com>
2021-10-05 23:23:33 +0300

    fixed binary operator indentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,66 @@
-import {useState} from "react"
+import { useState } from "react"
 
-// binary operators are always wrapped into 2 spaces.
+const DIGITS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const BINARY_OPERATORS = ["+", "*", "/", "%", "^"]
 
-const initialCalcString = ""
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-const binaryOperators = [ "+", "*", "/", "%", "^" ]
+/// shortcuts 
+// checks if char is digit
+const isDigit = char => /\d/.test(char)
+// gets string last character
+const lchar = str => str[str.length - 1]
+///
+
+// checks if string is valid bracket chain: 
+// = 0 => valid; < 0 => missing `(`; > 0 => missing `)`
+const getBracketChainOffset = str => {
+    let count = 0
+    for (const i in str) {
+        if (str[i] === "(")
+            count++
+        if (str[i] === ")") 
+            count--
+    }
+    return count
+}
 
 const specialOperators = {
-    "-": (str) => str[str.length - 1] === " " || str[str.length - 1] === "-", 
-    "(": (str) => { // fixme
-        const lastChar = str[str.length - 1]
-        return /\d/.test(lastChar)
-    },  
-    ")": (str) => {
-        let count = 0;
-        for (const i in str) {
-            if (str[i] === "(") {
-                count++
-            }
-            if (str[i] === ")") {
-                count--
-            }
-        }
-        return count <= 0
-        // check if valid bracket chain
+    "-": {
+        isAllowed: str => lchar(str) === " " || lchar(str) === "-",
+        onClick: str => isDigit(lchar(str)) ? `${str} - ` : (str + '-')
+    },
+    "(": {
+        isAllowed: str => isDigit(lchar(str)),
+        onClick: str => str + "("
+    },
+    ")": {
+        isAllowed: str => getBracketChainOffset(str) <= 0 || lchar(str) === "(" || lchar(str) === "-",
+        onClick: str => str + ")"
     }
 }
 
 function App() {
-    const [ calcString, setCalcString] = useState(initialCalcString)
+    const [ calcString, setCalcString] = useState("")
 
     const binaryOperatorBtn = operator => {
-        const strlen = calcString.length
-        const disabled = strlen === 0 || calcString[strlen - 1] === " "
+        const disabled = calcString.length === 0 || lchar(calcString) === " " || lchar(calcString) === "(" || lchar(calcString) === "-"
         const onClick = () => setCalcString(`${calcString} ${operator} `)
-        return (
-            <button 
-                onClick={onClick} 
-                disabled={disabled}
-            >
-                {operator}
-            </button>
-        )
+        return <button onClick={onClick} disabled={disabled}>{operator}</button>
     }
 
     const specialOperatorBtn = key => {
-        const onClick = () => setCalcString(calcString + key)
-        return (
-        <button 
-            onClick={onClick} 
-            disabled={specialOperators[key](calcString)}
-        >
-            {key}
-        </button>
-        )    
+        const onClick = () => setCalcString(specialOperators[key].onClick(calcString))
+        return <button onClick={onClick} disabled={specialOperators[key].isAllowed(calcString)}>{key}</button>
     }
 
     const numberBtn = number => {
         const onClick = () => setCalcString(calcString + number)
         return <button onClick={onClick}>{number}</button>
+    }
+
+    const onBackspace = () => {
+        const len = calcString.length
+        const stepsFromEnd = lchar(calcString) === " " ? 3 : 1
+        setCalcString(calcString.substr(0, len - stepsFromEnd))
     }
 
     return (
@@ -70,18 +72,11 @@ function App() {
                 value={calcString} 
                 onChange={event => setCalcString(event.target.value)
             }/>
-            {binaryOperators.map(operator => binaryOperatorBtn(operator))}
-            {numbers.map(number => numberBtn(number))}
+            {DIGITS.map(number => numberBtn(number))}
+            {BINARY_OPERATORS.map(operator => binaryOperatorBtn(operator))}
+            {Object.keys(specialOperators).map(key => specialOperatorBtn(key))}   
+            <button onClick={onBackspace}>Backspace</button>
             <button onClick={() => setCalcString("")}>C</button>
-            <button onClick={() => {
-                const len = calcString.length
-                const stepsFromEnd = calcString[len - 1] === " " ? 3 : 1
-                setCalcString(calcString.substr(0, len - stepsFromEnd))
-            }}>
-                Backspace
-            </button>
-
-            {Object.keys(specialOperators).map(key =>  specialOperatorBtn(key))}
         </div>
     )
 }
